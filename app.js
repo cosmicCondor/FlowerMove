1a44bfc73ade8a8143d27478eb1bfc421d9500d6
mfdezn <m.fernandezn.2021@alumnos.urjc.es>
2025-09-08 13:13:46 +0200

    Version 1.5.0

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // Credenciales
 const CREDENCIALES = {
   admin: { user: "Elisabeth", pass: "flowerElyRoses" },
